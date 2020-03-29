eff8ce97523c4ccd562e86da18f58d037f416852
Dhaval <prajapatidhaval304@gmail.com>
2020-03-29 20:48:53 +0530

    generate qr code done

--- app/src/main/java/com/example/project/PlanJourney.java
+++ app/src/main/java/com/example/project/PlanJourney.java
@@ -10,10 +10,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -97,21 +100,291 @@ public class PlanJourney extends AppCompatActivity implements NavigationView.OnN
         String src = sourceSpinner.getSelectedItem().toString();
         String des = destinationSpinner.getSelectedItem().toString();
         byte[] bytes;
+        Intent intent = new Intent(getApplicationContext(), ticketqr.class);
 
-        if(src.equals("Maninagar") && des.equals("Maninagar"))
+        if(src.equals("Maninagar") && des.equals("Shivranjani"))
         {
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maninagar_shivranjani);
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
             bytes = stream.toByteArray();
-            Toast.makeText(getApplicationContext(), src + "\n" + des, Toast.LENGTH_SHORT).show();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Maninagar") && des.equals("Nehrunagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maninagar_nehrunagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Maninagar") && des.equals("L.D College"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maninagar_ld);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Maninagar") && des.equals("Andhjan Mandal"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maninagar_andhjan);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
         }
         else if(src.equals("Maninagar") && des.equals("Iskcon"))
         {
-            Toast.makeText(getApplicationContext(), src + "\n" + des, Toast.LENGTH_SHORT).show();
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maninagar_iscon);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
         }
+        else if(src.equals("Shivranjani") && des.equals("Maninagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.shivranjani_maninagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Shivranjani") && des.equals("Nehrunagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.shivranjani_nehrunagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Shivranjani") && des.equals("L.D College"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.shivranjani_ld);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Shivranjani") && des.equals("Andhjan Mandal"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.shivranjani_andhjan);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Shivranjani") && des.equals("Iskcon"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.shivranjani_iscon);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Nehrunagar") && des.equals("Maninagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nehrunagar_maninagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Nehrunagar") && des.equals("Shivranjani"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nehrunagar_shivranjani);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Nehrunagar") && des.equals("L.D College"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nehrunagar_ld);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Nehrunagar") && des.equals("Andhjan Mandal"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nehrunagar_andhjan);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Nehrunagar") && des.equals("Iskcon"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nehrunagar_iscon);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("L.D College") && des.equals("Maninagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ld_maninagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("L.D College") && des.equals("Shivranjani"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ld_shivranjani);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("L.D College") && des.equals("Nehrunagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ld_nehrunagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("L.D College") && des.equals("Andhjan Mandal"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ld_andhjan);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("L.D College") && des.equals("Iskcon"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ld_iscon);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Andhjan Mandal") && des.equals("Maninagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.andhjan_maninagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Andhjan Mandal") && des.equals("Shivranjani"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.andhjan_shivranjani);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Andhjan Mandal") && des.equals("Nehrunagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.andhjan_nehrunagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Andhjan Mandal") && des.equals("L.D College"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.andhjan_ld);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Andhjan Mandal") && des.equals("Iskcon"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.andhjan_iscon);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Iskcon") && des.equals("Maninagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.iscon_maninagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Iskcon") && des.equals("Shivranjani"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.iscon_shivranjani);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Iskcon") && des.equals("Nehrunagar"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.iscon_nehrunagar);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Iskcon") && des.equals("L.D College"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.iscon_ld);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else if(src.equals("Iskcon") && des.equals("Andhjan Mandal"))
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.iscon_andhjan);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bytes = stream.toByteArray();
+            intent.putExtra("image", bytes);
+            startActivity(intent);
+        }
+        else
+        {
+            ShowToast("Couldn't find a route between those two places!!!");
+        }
+    }
 
-        /*Intent intent = new Intent(getApplicationContext(), ticketqr.class);
-        startActivity(intent);*/
+    private void ShowToast(String msg)
+    {
+        Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        View view = toast.getView();
+        view.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+        TextView text = view.findViewById(android.R.id.message);
+        text.setTextColor(Color.WHITE);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/project/ticketqr.java
+++ app/src/main/java/com/example/project/ticketqr.java
@@ -3,16 +3,31 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ticketqr extends AppCompatActivity
 {
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticketqr);
+
+        imageView = findViewById(R.id.qrImage);
+
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+        byte[] bytes = extras.getByteArray("image");
+
+        assert bytes != null;
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        imageView.setImageBitmap(bitmap);
     }
 
     public void addpic(View view)
